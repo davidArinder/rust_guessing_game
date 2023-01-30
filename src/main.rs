fcea003b9ebd47bc2591a614505e5c181f32e7db
David Arinder <david.arinder@detroitlabs.com>
2023-01-30 18:09:38 -0500

    added type conversion and number conmparison

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -18,5 +19,23 @@ fn main() {
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
+    // Rust lets you "shadow" the previous value of the same named variable. Shadowing
+    // lets us reuse the `guess` variable name rather than forcing us to create two unique
+    // variables to do type conversion (e.g., `guess_str` and 'guess).
+    // Also, the `trim` method removes whitespace before and after the string. We do this
+    // because the to use `read_line` the user presses `enter` which adds a new line character
+    // that must be removed.
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
+
     println!("You guessed: {}", guess);
+
+    // A `match` expression is make up of "arms". An arm consists of a "pattern" and
+    // the code that should be run if the value given to the beginning of the `match`
+    // expression fits that arm's pattern.
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
